6074b9714d2752ff5ef99c70c05ce8c827461071
Stan Verberkt <verberktstan@gmail.com>
2024-06-30 20:39:18 +0200

    Specify cljs version in project deps, exclude closure compiler unshaded

--- project.clj
+++ project.clj
@@ -9,7 +9,10 @@
   :profiles {:examples {:dependencies [[quil/quil "4.3.1563"]]
                         :source-paths ["src" "examples"]}
              :cljs {:source-paths ["src" "test"]
-                    :dependencies [#_[org.clojure/clojurescript "1.11.123"] ;; TODO: Make cljs dep explicit
+                    :dependencies [;; hack to get the latest closure-compiler if CLJS doesn't have it
+                                   [org.clojure/clojurescript "1.11.132"
+                                    :exclusions
+                                    [com.google.javascript/closure-compiler-unshaded]]
                                    [thheller/shadow-cljs "2.28.10"]]}}
 
   :aot :all)
